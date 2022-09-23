83ae5c5d76a68fe825a0ad0d8a26eeb25968c601
Rohit M L <112368030+mlr28@users.noreply.github.com>
2022-09-23 20:03:02 +0530

    Main Class Test

--- src/test/java/com/gof/design/GOFDesignStructureApplicationTests.java
+++ src/test/java/com/gof/design/GOFDesignStructureApplicationTests.java
@@ -1,13 +1,24 @@
 package com.gof.design;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class GOFDesignStructureApplicationTests {
 
+	@BeforeEach
+	void setUp(){
+
+	}
 	@Test
 	void contextLoads() {
+	}
+
+	@AfterEach
+	void tearDown(){
+
 	}
 
 }
