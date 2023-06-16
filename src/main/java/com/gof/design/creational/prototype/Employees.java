5a6e347c63cb1aef919e23349d6cd1cb7a65b780
Rohit Lakshmikanth <u127958@STRAUMANN.COM>
2023-06-16 22:30:00 +0530

    Creational Design Pattern - Prototype Pattern

---
+++ src/main/java/com/gof/design/creational/prototype/Employees.java
@@ -0,0 +1,37 @@
+package com.gof.design.creational.prototype;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Employees implements Cloneable{
+
+    private List<String> empList;
+
+    public Employees(){
+        empList = new ArrayList<>();
+    }
+
+    public Employees(List<String> list){
+        this.empList=list;
+    }
+    public void loadData(){
+        empList.add("Pankaj");
+        empList.add("Raj");
+        empList.add("Siraj");
+        empList.add("Rajath");
+    }
+
+    public List<String> getEmpList() {
+        return empList;
+    }
+
+    @Override
+    public Object clone() throws CloneNotSupportedException{
+        List<String> temp = new ArrayList<String>();
+        for(String s : this.getEmpList()){
+            temp.add(s);
+        }
+        return new Employees(temp);
+    }
+
+}
